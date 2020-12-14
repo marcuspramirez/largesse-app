c561e3b0b1a2b1abee70004c9e4c18a7cd0ec6ec
Adrian Banda <5205bda@gmail.com>
2020-12-13 22:38:20 -0600

    PASSPORT

--- schema.sql
+++ schema.sql
@@ -2,3 +2,9 @@
 DROP DATABASE IF EXISTS Impact;
 -- Creates the "blogger" database --
 CREATE DATABASE Impact;
+
+
+-- Drops the blogger if it exists currently --
+DROP DATABASE IF EXISTS passport_demo;
+-- Creates the "blogger" database --
+CREATE DATABASE passport_demo;
--- server.js
+++ server.js
@@ -5,6 +5,9 @@
 // *** Dependencies
 // =============================================================
 var express = require("express");
+var session = require("express-session");
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
 
 // Sets up the Express App
 // =============================================================
@@ -20,6 +23,14 @@ app.use(express.json());
 
 // Static directory
 app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+// We need to use sessions to keep track of our user's login status
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Routes
 // =============================================================
